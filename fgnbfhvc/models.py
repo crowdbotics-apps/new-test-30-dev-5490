4d878e906a7a123548b8811e805762189bd38ddc
Crowdbotics <team@crowdbotics.com>
2020-06-05 13:57:44 +0000

    Update models

---
+++ fgnbfhvc/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.db import models
+
+
+class Tgjf(models.Model):
+    "Generated Model"
+    cfgsf = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="tgjf_cfgsf",
+    )
+    sdasf = models.BigIntegerField(null=True, blank=True,)
+    safasdf = models.BigIntegerField(null=True, blank=True,)
+    erett = models.BigIntegerField(null=True, blank=True,)
+    tyjyuj = models.BigIntegerField(null=True, blank=True,)
+    ikiol = models.BigIntegerField(null=True, blank=True,)
+    qwsqwswqq = models.BigIntegerField(null=True, blank=True,)
+    wdwdwe = models.BigIntegerField(null=True, blank=True,)
+    dewdwee = models.BigIntegerField(null=True, blank=True,)
+    efrfrfrf = models.BigIntegerField(null=True, blank=True,)
+    erfrgth = models.BigIntegerField(null=True, blank=True,)
+
+
+# Create your models here.
